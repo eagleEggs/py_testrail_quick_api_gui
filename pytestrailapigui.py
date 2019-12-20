9834f59b38f777e4997905b9cc856e5ae8eb4bb4
eagleEggs <29800532+eagleEggs@users.noreply.github.com>
2019-12-20 12:27:32 -0500

    Moved PSG data to module

--- pytestrailapigui.py
+++ pytestrailapigui.py
@@ -1,16 +1,6 @@
 import testrail as tr
 import logging
-import PySimpleGUI as Sg
-
-dd_get_api = ['get_plan', 'get_plans', 'get_run',
-              'get_runs',  'get_test', 'get_cases',
-              'get_suite', 'get_suites', 'get_project',
-              'get_projects', 'get_statuses', 'get_tests']
-
-layout = [[Sg.DropDown(dd_get_api)]]
-main_layout = [[Sg.DropDown(dd_get_api, key='dd_get'),
-                  Sg.Text('Case #'), Sg.InputText(key='case_number'),
-                  Sg.Button("Request Data", key='get_data')]]
+import tr_test_data as trd
 
 
 class TestRail(object):
@@ -51,7 +41,7 @@ class TestRail(object):
 ok = TestRail("URL", "UN",
               "PW")
 
-main_window = Sg.Window(title='TR API TEST', layout=main_layout)
+main_window = trd.Sg.Window(title='TR API TEST', layout=trd.main_layout)
 
 while True:
     b, v = main_window.Read(timeout=0)
@@ -62,7 +52,7 @@ while True:
                                                      v['case_number']))
             print(get_data)
         except (tr.APIError) as error:
-            Sg.PopupOK('TestRail API Error', error)
+            trd.Sg.PopupOK('TestRail API Error', error)
 
     if b == 'exit' or None:
         quit()
